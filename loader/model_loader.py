c5771ebb4a81fcd91aac7193a3662a07444f3d31
Stewart Jamieson <sjamieson@whoi.edu>
2018-12-02 17:28:41 -0500

    Added: pretrainedmodels as a dependency, to be able to pull more test networks.

--- loader/model_loader.py
+++ loader/model_loader.py
@@ -4,19 +4,33 @@ import torchvision
 from nets.cornet.cornet_z import CORnet_Z
 from nets.cornet.cornet_r import CORnet_R
 from nets.cornet.cornet_s import CORnet_S
+import pretrainedmodels # pip install pretrainedmodels
 
 local_model_defs = { 'cornetz': CORnet_Z, 'cornetr': CORnet_R, 'cornets': CORnet_S }
 
+def getmodel(name, pretrained=True, dataset='imagenet', num_classes=1000):
+    if name in local_model_defs.keys():
+        return local_model_defs[name]()
+    elif pretrained:
+        if name in torchvision.models.__dict__ and dataset == 'imagenet':
+            return torchvision.models.__dict__[name](pretrained=True, num_classes=num_classes)
+        else:
+            return pretrainedmodels.__dict__[name](pretrained=dataset, num_classes=num_classes)
+    else:
+        if name in torchvision.models.__dict__:
+            return torchvision.models.__dict__[name](pretrained=False, num_classes=num_classes)
+        elif name in pretrainedmodels.__dict__:
+            return pretrainedmodels.__dict__[name](pretrained=None, num_classes=num_classes)
+        else:
+            raise FileNotFoundError('Failed to find model \'' + name + "'")
+
 def loadmodel(hook_fn):
     if settings.MODEL_FILE is None:
-        model = torchvision.models.__dict__[settings.MODEL](pretrained=True)
+        model = getmodel(settings.MODEL, pretrained=True, dataset=settings.DATASET, num_classes=settings.NUM_CLASSES)
     else:
         checkpoint = torch.load(settings.MODEL_FILE)
         if type(checkpoint).__name__ == 'OrderedDict' or type(checkpoint).__name__ == 'dict':
-            if settings.MODEL in local_model_defs.keys():
-                model = local_model_defs[settings.MODEL]()
-            else:
-                model = torchvision.models.__dict__[settings.MODEL](num_classes=settings.NUM_CLASSES)
+            model = getmodel(settings.MODEL, pretrained=False, num_classes=settings.NUM_CLASSES)
             if settings.MODEL_PARALLEL:
                 state_dict = {str.replace(k, 'module.', ''): v for k, v in checkpoint[
                     'state_dict'].items()}  # the data parallel layer will add 'module' before each layer name
