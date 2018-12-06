32c8f2b83c036df7d24864b6b8aedeb9d021c537
san-soucie <44901782+san-soucie@users.noreply.github.com>
2018-12-06 18:39:04 -0500

    Alexnet over densenet

--- processing/process_synthesized_images.py
+++ processing/process_synthesized_images.py
@@ -11,11 +11,11 @@ class NetSoftmax(nn.Module):
 
     def __init__(self, num_classes=1000, aux_logits=True, transform_input=False):
         super(NetSoftmax, self).__init__()
-        self.net = torchvision.models.densenet169(pretrained=True,
+        self.net = torchvision.models.alexnet(pretrained=True,
                                             num_classes=num_classes)
 #                                            aux_logits=aux_logits,
 #                                            transform_input=transform_input)
-        self.net.classifier = nn.Linear(self.net.classifier.in_features * 2, num_classes)
+#        self.net.classifier = nn.Linear(self.net.classifier.in_features*81, num_classes)
         self.sm = nn.Softmax()
 
     def forward(self, x):
@@ -65,7 +65,11 @@ def find_classes(class_filename):
 
 class CustomDataset(torch.utils.data.Dataset):
 
-    def __init__(self, folder_name, transform=None, class_filename="imagenet_classes.txt", stopwords=None):
+    def __init__(self,
+                 folder_name,
+                 transform=None,
+                 class_filename="imagenet_classes.txt",
+                 stopwords=None):
 
         classes, class_to_idx = find_classes(class_filename)
 
@@ -112,12 +116,12 @@ class CustomDataset(torch.utils.data.Dataset):
 
 def main():
     parser = argparse.ArgumentParser(description="Creates a scatterplot of scores vs. softmax probabilities")
-    parser.add_argument("image_folder_name",
+    parser.add_argument("image_folder_name", default="/home/warp/Documents/6.861/NetDissect-Lite/outputs/synthesizing-06:52:34/CORnet-S/clean",
                         metavar="F", help="Folder containing images to be parsed")
     parser.add_argument("-T", "--network_name",
-                        metavar="T", default="", help="Name of neural network generating synthesized images")
+                        metavar="T", default="CORnet-S", help="Name of neural network generating synthesized images")
     parser.add_argument("-O", "--output_image_filename",
-                        metavar="F", default="", help="Filename for output image")
+                        metavar="F", default="CORnet-S_Clean_MaxSofmax_vs_IoU.pdf", help="Filename for output image")
     args = parser.parse_args()
     args.image_folder_names = []
     if args.image_folder_name == "LIST":
@@ -134,32 +138,39 @@ def main():
     if not args.image_folder_names:
         dataset = CustomDataset(args.image_folder_name,
                                 transform=torchvision.transforms.ToTensor(),
-                                stopwords={'CORnet'})
+                                stopwords={})
         dataloader = torch.utils.data.DataLoader(dataset)
         net = NetSoftmax()
-        softmax_scores = []
-        iou_scores = []
+        softmax_scores_object = []
+        iou_scores_object = []
         softmax_scores_bad = []
         iou_scores_bad = []
+        softmax_scores_texture = []
+        iou_scores_texture = []
         with torch.no_grad():
             for image, score, path in dataloader:
                 softmaxes = net.forward(image)
                 if '_bad.' in path[0]:
                     softmax_scores_bad.append(max(softmaxes.tolist()[0]))
                     iou_scores_bad.append(score[0].item())
+                elif "_object_" in path[0]:
+                    softmax_scores_object.append(max(softmaxes.tolist()[0]))
+                    iou_scores_object.append(score[0].item())
                 else:
-                    softmax_scores.append(max(softmaxes.tolist()[0]))
-                    iou_scores.append(score[0].item())
+                    softmax_scores_texture.append(max(softmaxes.tolist()[0]))
+                    iou_scores_texture.append(score[0].item())
     else:
         datasets = [CustomDataset(x,
                                   transform=torchvision.transforms.ToTensor(),
                                   stopwords={'CORnet'}) for x in args.image_folder_names]
         dataloaders = [torch.utils.data.DataLoader(d) for d in datasets]
         net = NetSoftmax()
-        softmax_scores = []
-        iou_scores = []
+        softmax_scores_object = []
+        iou_scores_object = []
         softmax_scores_bad = []
         iou_scores_bad = []
+        softmax_scores_texture= []
+        iou_scores_texture = []
         with torch.no_grad():
             for d in dataloaders:
                 for image, score, path in d:
@@ -167,22 +178,22 @@ def main():
                     if '_bad.' in path[0]:
                         softmax_scores_bad.append(max(softmaxes.tolist()[0]))
                         iou_scores_bad.append(score[0].item())
+                    elif "_object_" in path[0]:
+                        softmax_scores_object.append(max(softmaxes.tolist()[0]))
+                        iou_scores_object.append(score[0].item())
                     else:
-                        softmax_scores.append(max(softmaxes.tolist()[0]))
-                        iou_scores.append(score[0].item())
-    plt.plot(iou_scores, softmax_scores, 'bo', label="Top-5 Unit")
+                        softmax_scores_texture.append(max(softmaxes.tolist()[0]))
+                        iou_scores_texture.append(score[0].item())
+    plt.plot(iou_scores_object, softmax_scores_object, 'bo', label="Top-5 Unit (Object)")
+    plt.plot(iou_scores_texture, softmax_scores_texture, 'go', label="Top-5 Unit (Texture)")
     plt.plot(iou_scores_bad, softmax_scores_bad, 'ro', label="Bottom-5 Unit")
 
     if args.network_name:
         plt.title("Maximum softmax probability vs. IoU score (" + args.network_name + ")")
     else:
         plt.title("Maximum softmax probability vs. IoU score")
-    x_buffer = (max(iou_scores) - min(iou_scores)) / 20
-    y_buffer = (max(softmax_scores + softmax_scores_bad) - min(softmax_scores)) / 20
-    plt.xlim(left=min(iou_scores + iou_scores_bad) - x_buffer,
-             right=max(iou_scores + iou_scores_bad) + x_buffer)
-    plt.ylim(bottom=min(softmax_scores + iou_scores_bad) - y_buffer,
-             top=max(softmax_scores) + y_buffer)
+    plt.xlim(left=0, right=0.6)
+    plt.ylim(bottom=0, top=1)
     plt.xlabel('Unit IoU Score')
     plt.ylabel('Maximum Softmax Score')
     plt.legend()
