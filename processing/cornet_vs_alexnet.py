40d49d2c28a743007c2e6f23688155644cca4f0f
san-soucie <j.e.sansoucie@gmail.com>
2018-12-07 03:22:54 -0500

    Refactored cornet_vs_alexnet

--- processing/cornet_vs_alexnet.py
+++ processing/cornet_vs_alexnet.py
@@ -53,143 +53,94 @@ cornet_s = cornet_s.CORnet_S()
 cornet_s.load_state_dict({k[7:]: v for k, v in torch.load("../zoo/cornet_s_epoch43.pth.tar")["state_dict"].items()})
 cornet_z = cornet_z.CORnet_Z()
 cornet_z.load_state_dict({k[7:]: v for k, v in torch.load("../zoo/cornet_z_epoch25.pth.tar")["state_dict"].items()})
+
 net.eval()
 cornet_s.eval()
 cornet_z.eval()
 
+def plot_fig(alexnet,
+             cornet_model,
+             cornet_dataloader,
+             cornet_dataloader_no_norm,
+             net_name):
+    softmax_scores_object = []
+    iou_scores_object = []
+    cornet_scores_object = []
+    cornet_max_index_object = []
+    softmax_scores_bad = []
+    iou_scores_bad = []
+    cornet_scores_bad = []
+    cornet_max_index_bad= []
+    softmax_scores_texture = []
+    iou_scores_texture = []
+    cornet_scores_texture= []
+    cornet_max_index_texture = []
 
-softmax_scores_object = []
-iou_scores_object = []
-cornet_scores_object = []
-cornet_max_index_object = []
-softmax_scores_bad = []
-iou_scores_bad = []
-cornet_scores_bad = []
-cornet_max_index_bad= []
-softmax_scores_texture = []
-iou_scores_texture = []
-cornet_scores_texture= []
-cornet_max_index_texture = []
+    with torch.no_grad():
+        cornet_model = cornet_z.cuda()
+        cornet_dataloader = cornetz_dataloader
+        cornet_dataloader_no_norm = cornetz_dataloader_no_norm
 
-with torch.no_grad():
-    cornet_model = cornet_s.cuda()
-    cornet_dataloader = cornets_dataloader
-    cornet_dataloader_no_norm = cornets_dataloader_no_norm
+        alexnet = alexnet.cuda()
+        for no_norm, norm in zip(cornet_dataloader_no_norm, cornet_dataloader):
+            image_no_norm, score, path = no_norm
+            image_norm, _, _ = norm
+            data_no_norm = image_no_norm.cuda()
+            data_norm = image_norm.cuda()
+            cornet_softmax = net.forward(data_no_norm).cpu()
+            softmaxes = alexnet.forward(data_norm).cpu()
+            val, idx = cornet_softmax.squeeze().max(0)
 
-    net = net.cuda()
-    for no_norm, norm in zip(cornet_dataloader_no_norm, cornet_dataloader):
-        image_no_norm, score, path = no_norm
-        image_norm, _, _ = norm
-        data_no_norm = image_no_norm.cuda()
-        data_norm = image_norm.cuda()
-        cornet_softmax = net.forward(data_no_norm).cpu()
-        softmaxes = net.forward(data_norm).cpu()
-        val, idx = cornet_softmax.squeeze().max(0)
-        if '_bad.' in path[0]:
-            cornet_scores_bad.append(float(val))
-            iou_scores_bad.append(score[0].item())
-            cornet_max_index_bad.append(int(idx))
-            softmax_scores_bad.append(softmaxes.tolist()[0][idx])
-        elif "_object_" in path[0]:
-            cornet_scores_object.append(float(val))
-            iou_scores_object.append(score[0].item())
-            cornet_max_index_object.append(idx)
-            softmax_scores_object.append(softmaxes.tolist()[0][idx])
-        else:
-            cornet_scores_texture.append(float(val))
-            iou_scores_texture.append(score[0].item())
-            cornet_max_index_texture.append(idx)
-            softmax_scores_texture.append(softmaxes.tolist()[0][idx])
+            if '_bad.' in path[0]:
+                cornet_scores_bad.append(float(val))
+                iou_scores_bad.append(score[0].item())
+                cornet_max_index_bad.append(int(idx))
+                softmax_scores_bad.append(softmaxes.tolist()[0][idx])
+            elif "_object_" in path[0]:
+                cornet_scores_object.append(float(val))
+                iou_scores_object.append(score[0].item())
+                cornet_max_index_object.append(idx)
+                softmax_scores_object.append(softmaxes.tolist()[0][idx])
+            else:
+                cornet_scores_texture.append(float(val))
+                iou_scores_texture.append(score[0].item())
+                cornet_max_index_texture.append(idx)
+                softmax_scores_texture.append(softmaxes.tolist()[0][idx])
 
-plt.plot(iou_scores_object, softmax_scores_object, 'bo', label="Top-5 Unit (Object)")
-plt.plot(iou_scores_texture, softmax_scores_texture, 'go', label="Top-5 Unit (Texture)")
-plt.plot(iou_scores_bad, softmax_scores_bad, 'ro', label="Bottom-5 Unit")
+    plt.clf()
+    plt.plot(iou_scores_object, softmax_scores_object, 'bo', label="Top-5 Unit (Object)")
+    plt.plot(iou_scores_texture, softmax_scores_texture, 'go', label="Top-5 Unit (Texture)")
+    plt.plot(iou_scores_bad, softmax_scores_bad, 'ro', label="Bottom-5 Unit")
 
-gradient, intercept, r_value, p_value, std_err = linregress(
-    iou_scores_bad + iou_scores_object + iou_scores_texture,
-    softmax_scores_bad + softmax_scores_object + softmax_scores_texture)
+    gradient, intercept, r_value, p_value, std_err = linregress(
+        iou_scores_bad + iou_scores_object + iou_scores_texture,
+        softmax_scores_bad + softmax_scores_object + softmax_scores_texture)
 
-xs = np.linspace(0.01, 0.99, 98)
-ys = gradient * xs + intercept
-plt.plot(xs, ys, c='k')
-plt.gca().annotate("r = %3.5f\np = %3.5f" % (r_value, p_value), (0.01, 0.9))
+    xs = np.linspace(0.01, 0.99, 98)
+    ys = gradient * xs + intercept
+    plt.plot(xs, ys, c='k')
+    plt.gca().annotate("r = %3.5f\np = %3.5f" % (r_value, p_value), (0.01, 0.9))
 
-plt.title("AlexNet Softmax of CORnet-S Maximal Activators")
+    plt.title("AlexNet Softmax of " + net_name + " Maximal Activators")
 
-plt.xlim(left=0, right=0.5)
-plt.ylim(bottom=-0.1, top=1)
-plt.xlabel('CORnet-S Unit IoU Score')
-plt.ylabel('AlexNet Softmax')
-plt.legend()
-img_filename = "CORnet-S_Alexnet_softmax_vs_iou.pdf"
-plt.savefig(img_filename, bbox_inches='tight')
+    plt.xlim(left=0, right=0.5)
+    plt.ylim(bottom=-0.1, top=1)
+    plt.xlabel('CORnet-Z Unit IoU Score')
+    plt.ylabel('AlexNet Softmax')
+    plt.legend()
+    img_filename = net_name + "x_Alexnet_softmax_vs_iou.pdf"
+    plt.savefig(img_filename, bbox_inches='tight')
 
 
-softmax_scores_object = []
-iou_scores_object = []
-cornet_scores_object = []
-cornet_max_index_object = []
-softmax_scores_bad = []
-iou_scores_bad = []
-cornet_scores_bad = []
-cornet_max_index_bad= []
-softmax_scores_texture = []
-iou_scores_texture = []
-cornet_scores_texture= []
-cornet_max_index_texture = []
+plot_fig(net,
+         cornet_s.cuda(),
+         cornets_dataloader,
+         cornets_dataloader_no_norm,
+         "CORnet-S")
 
-with torch.no_grad():
-    cornet_model = cornet_z.cuda()
-    cornet_dataloader = cornetz_dataloader
-    cornet_dataloader_no_norm = cornetz_dataloader_no_norm
-
-    net = net.cuda()
-    for no_norm, norm in zip(cornet_dataloader_no_norm, cornet_dataloader):
-        image_no_norm, score, path = no_norm
-        image_norm, _, _ = norm
-        data_no_norm = image_no_norm.cuda()
-        data_norm = image_norm.cuda()
-        cornet_softmax = net.forward(data_no_norm).cpu()
-        softmaxes = net.forward(data_norm).cpu()
-        val, idx = cornet_softmax.squeeze().max(0)
-
-        if '_bad.' in path[0]:
-            cornet_scores_bad.append(float(val))
-            iou_scores_bad.append(score[0].item())
-            cornet_max_index_bad.append(int(idx))
-            softmax_scores_bad.append(softmaxes.tolist()[0][idx])
-        elif "_object_" in path[0]:
-            cornet_scores_object.append(float(val))
-            iou_scores_object.append(score[0].item())
-            cornet_max_index_object.append(idx)
-            softmax_scores_object.append(softmaxes.tolist()[0][idx])
-        else:
-            cornet_scores_texture.append(float(val))
-            iou_scores_texture.append(score[0].item())
-            cornet_max_index_texture.append(idx)
-            softmax_scores_texture.append(softmaxes.tolist()[0][idx])
-
-plt.clf()
-plt.plot(iou_scores_object, softmax_scores_object, 'bo', label="Top-5 Unit (Object)")
-plt.plot(iou_scores_texture, softmax_scores_texture, 'go', label="Top-5 Unit (Texture)")
-plt.plot(iou_scores_bad, softmax_scores_bad, 'ro', label="Bottom-5 Unit")
-
-gradient, intercept, r_value, p_value, std_err = linregress(
-    iou_scores_bad + iou_scores_object + iou_scores_texture,
-    softmax_scores_bad + softmax_scores_object + softmax_scores_texture)
-
-xs = np.linspace(0.01, 0.99, 98)
-ys = gradient * xs + intercept
-plt.plot(xs, ys, c='k')
-plt.gca().annotate("r = %3.5f\np = %3.5f" % (r_value, p_value), (0.01, 0.9))
-
-plt.title("AlexNet Softmax of CORnet-Z Maximal Activators")
-
-plt.xlim(left=0, right=0.5)
-plt.ylim(bottom=-0.1, top=1)
-plt.xlabel('CORnet-Z Unit IoU Score')
-plt.ylabel('AlexNet Softmax')
-plt.legend()
-img_filename = "CORnet-Z_Alexnet_softmax_vs_iou.pdf"
-plt.savefig(img_filename, bbox_inches='tight')
+# plot_fig(net,
+#          cornet_z.cuda(),
+#          cornetz_dataloader,
+#          cornetz_dataloader_no_norm,
+#          "CORnet-Z")
 
